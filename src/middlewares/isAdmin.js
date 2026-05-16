19b8ce21ab926aed9913ee1344ce63aff00375a3
Trung <nguyennghiatrung127@gmail.com>
2026-05-16 15:19:11 +0700

    add profile routes

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,6 +1,27 @@
 module.exports = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Forbidden" });
+  try {
+    console.log("USER:", req.user);
+
+    // chưa đăng nhập hoặc token lỗi
+    if (!req.user) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    // không phải admin
+    if (req.user.role !== "admin") {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+
+    next();
+  } catch (err) {
+    console.log("isAdmin error:", err);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
   }
-  next();
 };

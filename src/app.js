1f6de024077626c67a01790c947360289a9db46b
Trung <nguyennghiatrung127@gmail.com>
2026-06-12 01:57:20 +0700

    ffixxx

--- src/app.js
+++ src/app.js
@@ -15,7 +15,25 @@ const notificationRoutes =
     "./routes/notificationRoutes"
   );
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "DELETE",
+      "OPTIONS",
+    ],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+    ],
+  })
+);
+
+app.options("*", cors());
+
 app.use(express.json());
 
 // API routes
